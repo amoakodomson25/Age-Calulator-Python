aa81878b7dd55499eb98ebad4bc37f010a66ec62
Amoako Domson Wahab <wamoako_domson@st.ug.edu.gh>
2024-03-28 22:17:46 +0000

    Update app.py
    Added error handlers.

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import datetime 
-
-day = int(input("Day of birth: "))
-month = int(input("Month of birth: "))
-year = int(input("Year of birth: "))
-DOB = datetime.date(year,month,day)
-today = datetime.date.today()
-
-
-
-years = today.year - DOB.year - ((today.month,today.day)<(DOB.month,DOB.day))
-months = abs(today.month - DOB.month)
-days =abs(today.day - DOB.day)
-print()
-print(f"You are {years}years, {months}months and {days}days old.")
-print()
+try:
+    print()
+    day = int(input("Enter day of birth: "))
+    month = int(input("Enter month of birth: "))
+    year = int(input("Enter year of birth: "))
+    DOB = datetime.date(year,month,day)
+    today = datetime.date.today()
+    years = today.year - DOB.year - ((today.month,today.day)<(DOB.month,DOB.day))
+    months = abs(today.month - DOB.month)
+    days =abs(today.day - DOB.day)
+    print()
+    print(f"You are {years}years, {months}months and {days}days old.")
+    print()
+except ValueError:
+    print()
+    print("Invalid input, Please enter a valid integer")
+    print()
